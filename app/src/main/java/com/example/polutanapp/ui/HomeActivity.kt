d149c114b3e4b80383e4cf57f2fe8334efa3e319
ptrcksng106 <sinagapatrick610@gmail.com>
2022-06-08 02:07:59 +0700

    add bottom nav bar, use fragment for the home navbar

--- app/src/main/java/com/example/polutanapp/ui/HomeActivity.kt
+++ app/src/main/java/com/example/polutanapp/ui/HomeActivity.kt
@@ -2,12 +2,28 @@ package com.example.polutanapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.polutanapp.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
         supportActionBar?.hide()
+
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration.Builder(
+            R.id.navigation_home, R.id.navigation_air_quality, R.id.navigation_about_us
+        ).build()
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
 }
