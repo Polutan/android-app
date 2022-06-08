0d3777140f02c0bca132bc534681b618be0b9ce9
ptrcksng106 <sinagapatrick610@gmail.com>
2022-06-09 01:09:56 +0700

    add ui for setting

---
+++ app/src/main/java/com/example/polutanapp/ui/ListAllCityActivity.kt
@@ -0,0 +1,12 @@
+package com.example.polutanapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.polutanapp.R
+
+class ListAllCityActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_all_city2)
+    }
+}
